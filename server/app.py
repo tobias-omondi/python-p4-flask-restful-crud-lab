0aef339586e4787925bcde3c20bc3584d694f3fb
tobias <tobias.omondi@student.moringaschool.com>
2023-11-05 14:44:33 +0300

    updated

--- server/app.py
+++ server/app.py
@@ -46,6 +46,37 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant,attr,data[attr])
+            db.session.add(plant)
+            db.session.commit()
+
+            plant_serialized = plant.to_dict()
+
+            response = make_response(
+                jsonify(plant_serialized),
+                200
+            )
+            return response
+        
+        
+    def delete (self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        response_dict = {}
+
+        response = make_response(
+            jsonify(response_dict),
+            204
+        )
+
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
